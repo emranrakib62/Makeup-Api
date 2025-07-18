0fd3001dff6caeedae3154bf6dde99de3931f977
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-18 14:37:27 +0600

    updated

--- app/src/main/java/com/example/makeupapi/ProductService.kt
+++ app/src/main/java/com/example/makeupapi/ProductService.kt
@@ -1,4 +1,9 @@
 package com.example.makeupapi
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ProductService {
+    @GET("products.json")
+    fun getAllProduct(): Call<List<ResponseProduct>>
 }
